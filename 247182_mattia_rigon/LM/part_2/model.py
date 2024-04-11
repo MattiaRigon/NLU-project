c5abf791955518b3376c51a8c2114fec8eda9684
MattiaRigon <rigonmattia@gmail.com>
2024-04-11 10:48:33 +0000

    add variational dropout

--- 247182_mattia_rigon/LM/part_2/model.py
+++ 247182_mattia_rigon/LM/part_2/model.py
@@ -4,24 +4,53 @@ import torch.optim as optim
 import torch.nn.functional as F
 import math
 import numpy as np
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+from utils import DEVICE
 
 class VariationalDropout(nn.Module):
   def __init__(self,dropout_rate):
     super(VariationalDropout, self).__init__()
     self.dropout_rate = dropout_rate
 
+  def __mask_data(self,data):
+
+    """
+      data: [batch_size, length_of_sequence, layers_dimentions ]      
+    """
+
+    binomial = torch.distributions.binomial.Binomial(probs=1-self.dropout_rate)
+    m = binomial.sample((data.shape[0],1,data.shape[2])).to(DEVICE)
+    mask = m.expand(data.shape[0],data.shape[1],data.shape[2])
+    masked_data = mask * data * (1/(1-self.dropout_rate))
+    return masked_data
+
+
   def forward(self, x):
+
+    """
+      x: [batch_size, length_of_sequence, layers_dimentions ]      
+    """
 
     if not self.training:
       return x
-    
+
+    data = x
+    pack_length = 1  
+    is_packed = False
+
     if isinstance(x, nn.utils.rnn.PackedSequence):
-      mask = torch.bernoulli((1-self.dropout_rate) * torch.ones(x.data.size(0), x.data.size(1), x.data.size(2))).to(x.data.device)
-      masked_data = mask.unsqueeze(2) * x.data * (1.0/(1-self.dropout_rate))
-      return nn.utils.rnn.PackedSequence(masked_data, x.batch_sizes)
-    else:
-      mask = torch.bernoulli((1-self.dropout_rate) * torch.ones(x.size(0), x.size(1), x.size(2))).to(x.device)
-      return mask.unsqueeze(2) * x * (1.0/(1-self.dropout_rate))
+      is_packed = True
+      data, pack_length = pad_packed_sequence(x, batch_first=True)
+
+    data = self.__mask_data(data)
+
+    if is_packed:
+      return pack_padded_sequence(data,pack_length)
+    else :
+      return data
+
+
 
 class LSTM(nn.Module):
   def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,emb_dropout=0.1, n_layers=1):
@@ -32,13 +61,16 @@ class LSTM(nn.Module):
     self.pad_token = pad_index
     self.dropout2 = nn.Dropout(p=out_dropout)
     self.output = nn.Linear(hidden_size, output_size)
+    self.variationalDropout = VariationalDropout(out_dropout)
     assert emb_size == hidden_size
     self.output.weight = self.embedding.weight # Apply weight tying
 
   def forward(self, input_sequence):
     emb = self.embedding(input_sequence)
-    drp1 = self.dropout1(emb)
-    lstm_out, _  = self.lstm(drp1)
-    drp2 = self.dropout2(lstm_out)
-    output = self.output(drp2).permute(0,2,1)
+    # drp1 = self.dropout1(emb)
+    var_drop1 = self.variationalDropout(emb)
+    lstm_out, _  = self.lstm(var_drop1)
+    var_drop2 = self.variationalDropout(lstm_out)
+    # drp2 = self.dropout2(var_drop2)
+    output = self.output(var_drop2).permute(0,2,1)
     return output
