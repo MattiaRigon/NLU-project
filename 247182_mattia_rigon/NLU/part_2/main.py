445f1a96a21e867b413f290d460a9caa78967b4b
MattiaRigon <rigonmattia@gmail.com>
2024-04-28 10:58:27 +0200

    first setup for Bert

--- 247182_mattia_rigon/NLU/part_2/main.py
+++ 247182_mattia_rigon/NLU/part_2/main.py
@@ -2,8 +2,38 @@
 # Please write your fuctions or classes in the functions.py
 
 # Import everything from functions.py file
+from collections import Counter
+import os
 from functions import *
+from transformers import BertTokenizer, BertModel
+from pprint import pprint
+from utils import IntentsAndSlots, collate_fn, load_data, Lang
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+
+
 
 if __name__ == "__main__":
     #Wrtite the code to load the datasets and to run your functions
     # Print the results
+    
+    # Load the data
+    train_raw = load_data(os.path.join('dataset','ATIS','train.json'))
+    dev_raw = load_data(os.path.join('dataset','ATIS','dev.json'))
+    test_raw = load_data(os.path.join('dataset','ATIS','test.json'))
+
+    train_dataset = IntentsAndSlots(train_raw, lang=None)
+    dev_dataset = IntentsAndSlots(dev_raw, lang=None)
+    test_dataset = IntentsAndSlots(test_raw, lang=None)
+
+    train_loader = DataLoader(train_dataset, batch_size=128, collate_fn=collate_fn,  shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
+
+    model = BertModel.from_pretrained("bert-base-uncased") # Download the model
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased") # Download the tokenizer
+
+    
+
+
+    
---
+++ 247182_mattia_rigon/NLU/part_2/model.py
@@ -0,0 +1,41 @@
+import torch.nn as nn
+from transformers import BertTokenizer, BertModel
+
+class JointIntentSlotsBert(nn.module):
+
+    def __init__(self,out_slot,out_int,dropout=0.1,):
+
+        super(JointIntentSlotsBert, self).__init__()
+
+        # Carica il modello BERT pre-addestrato e il tokenizer
+        self.bert_model = BertModel.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+        self.slot_out = nn.Linear(self.bert_model.config.hidden_size, out_slot)  # Update from 200 to 400
+        self.intent_out = nn.Linear(self.bert_model.config.hidden_size, out_int)  # Update from 200 to 400
+        # Dropout layer How/Where do we apply it?
+        self.dropout = nn.Dropout(dropout)
+
+
+    def forward(self,inputs):
+
+        bert_output = self.bert_model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
+
+        # Estrai gli embeddings dall'ultimo layer di BERT
+        bert_embeddings = bert_output.last_hidden_state
+
+        # slot_output = self.slot_out(bert_embeddings[:, 0, :])  # Assuming you're using the CLS token for classification
+        # intent_output = self.intent_out(bert_embeddings[:, 0, :])  # Assuming you're using the CLS token for classification
+
+        pooled_output = bert_output[1]  # [CLS]
+        sequence_output = bert_output[0]
+
+        intent_output = self.intent_classifier(pooled_output)
+        slot_output = self.slot_classifier(sequence_output)
+
+        ## SOFTMAX ? 
+        
+
+        return slot_output, intent_output
+
+        
