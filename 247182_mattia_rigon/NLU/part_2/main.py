ff429525931e1620c1e8ca8342312495d05195ad
MattiaRigon <rigonmattia@gmail.com>
2024-04-30 09:32:20 +0200

    update for Bert

--- 247182_mattia_rigon/NLU/part_2/main.py
+++ 247182_mattia_rigon/NLU/part_2/main.py
@@ -5,35 +5,26 @@
 from collections import Counter
 import os
 from functions import *
-from transformers import BertTokenizer, BertModel
 from pprint import pprint
 from utils import IntentsAndSlots, collate_fn, load_data, Lang
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader
+from transformers import BertTokenizer
 
-
+from model import JointIntentSlotsBert
 
 if __name__ == "__main__":
-    #Wrtite the code to load the datasets and to run your functions
-    # Print the results
     
     # Load the data
     train_raw = load_data(os.path.join('dataset','ATIS','train.json'))
     dev_raw = load_data(os.path.join('dataset','ATIS','dev.json'))
     test_raw = load_data(os.path.join('dataset','ATIS','test.json'))
 
-    train_dataset = IntentsAndSlots(train_raw, lang=None)
-    dev_dataset = IntentsAndSlots(dev_raw, lang=None)
-    test_dataset = IntentsAndSlots(test_raw, lang=None)
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    train_loader = DataLoader(train_dataset, batch_size=128, collate_fn=collate_fn,  shuffle=True)
-    dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
+    train_loader = DataLoader(train_raw, batch_size=128, collate_fn=lambda x: collate_fn(x, tokenizer), shuffle=True)
+    dev_loader = DataLoader(dev_raw, batch_size=64, collate_fn=lambda x: collate_fn(x, tokenizer))
+    test_loader = DataLoader(test_raw, batch_size=64, collate_fn=lambda x: collate_fn(x, tokenizer))
 
-    model = BertModel.from_pretrained("bert-base-uncased") # Download the model
-    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased") # Download the tokenizer
-
-    
-
-
+    model = JointIntentSlotsBert(out_slot=128, out_int=22, dropout=0.1)
     
